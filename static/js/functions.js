96a98d1424f719fd6bcbaccdfc5a46d36447398a
NicoSun <nico.sun@pm.me>
2023-05-26 22:10:14 +0100

    static file restructure

---
+++ static/js/functions.js
@@ -0,0 +1,28 @@
+// Toggle dark mode
+darkmode()
+function darkmode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+
+// Used to toggle Info
+function more_info() {
+  var x = document.getElementById("more_info");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
